a5843639fc9d822c6845e9af39b668aff2cb96f4
gersonhiggins <gahg.10gerson@gmail.com>
2023-04-11 16:10:10 -0400

    Add Js Linters And CheckOut

--- scripts.js
+++ scripts.js
@@ -1,29 +1,27 @@
+const hamburguer = document.querySelector('.breadcrumb');
+const navMenu = document.querySelector('.desktop-nav');
+const section = document.querySelector('.section');
 
-		const hamburguer = document.querySelector('.breadcrumb');
-		const navMenu = document.querySelector('.desktop-nav');
-		const section = document.querySelector('.section');
-
-		hamburguer.addEventListener('click', () => {
+hamburguer.addEventListener('click', () => {
 		  hamburguer.classList.toggle('active');
 		  navMenu.classList.toggle('active');
-		});
+});
 
-		document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
 		  hamburguer.classList.remove('active');
 		  navMenu.classList.remove('active');
-		}));
-		function showPopUp(id) {		  
-			const popup = document.querySelector(".pop-"+id);
+}));
+function showPopUp(id) {
+  const popup = document.querySelector(`.pop-${id}`);
 		  	popup.classList.toggle('splash');
-			section.classList.toggle("blur");
-
-		}
-		function validateForm(e){
-  			let mail = document.forms["contact-form"]["email"].value;
-			const error = document.getElementById('mail-error');
-  			if(mail.toLowerCase() != mail){
+  section.classList.toggle('blur');
+}
+function validateForm(e) {
+  			const mail = document.forms['contact-form'].email.value;
+  const error = document.getElementById('mail-error');
+  			if (mail.toLowerCase() != mail) {
   				error.hidden = false;
-  				return false;  				
+  				return false;
   			}
-  			return true
-		}
+  			return true;
+}
