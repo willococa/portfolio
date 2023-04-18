ad37607d4f9848a4672e0b8d98dfedcf1b842bb0
kirube1992 <kirubelaynalem@gmail.com>
2023-04-18 17:08:54 -0300

    Add js file

---
+++ index.js
@@ -0,0 +1,11 @@
+const hamburguer = document.querySelector('.toggle');
+const Menu = document.querySelector('.menu2');
+hamburguer.addEventListener('click', () => {
+  hamburguer.classList.toggle('active');
+  Menu.classList.toggle('active');
+});
+
+document.querySelectorAll('.item').forEach((n) => n.addEventListener('click', () => {
+  hamburguer.classList.remove('active');
+  main.classList.remove('active');
+}));
