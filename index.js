a610fd918a5c42e5c0f4af46ffa567b1c11af08a
willocoro <willocoro25@gmail.com>
2023-04-19 13:26:47 -0500

    linters fix

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const menu = document.querySelector('.desktop-nav');
 hamburguer.addEventListener('click', () => {
   hamburguer.classList.toggle('active');
   menu.classList.toggle('active');
-  document.body.classList.toggle("inactive")
+  document.body.classList.toggle('inactive');
 });
 
 document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
- 
   hamburguer.classList.toggle('active');
-  menu.classList.toggle('active');  
-  document.body.classList.toggle("inactive")
+  menu.classList.toggle('active');
+  document.body.classList.toggle('inactive');
 }));
